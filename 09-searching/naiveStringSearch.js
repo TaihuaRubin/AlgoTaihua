0681e02e6c4530799c1b72c872f5d3d5bb3fedb6
Taihua Rubin <TaihuaRubin@gmail.com>
2020-10-20 22:07:43 -0400

    naive string search

---
+++ 09-searching/naiveStringSearch.js
@@ -0,0 +1,16 @@
+const naiveStringSearch = (long, short) => {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) {
+        break;
+      }
+      if (j === short.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(naiveStringSearch("lolie loled", "lol"));
