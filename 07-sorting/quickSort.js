add0ab325cefd9b97c6c47a937e29f5d2ed9f8df
Taihua Rubin <TaihuaRubin@gmail.com>
2020-10-20 22:49:50 -0400

    quick sort

---
+++ 07-sorting/quickSort.js
@@ -0,0 +1,73 @@
+/** Quick Sort */
+
+/** Pivot helper
+ *
+ * 1. All values less than the pivot are moved to the left and all values greater than the pivot are moved to the right.
+ * But the order on either side doesn't matter. It just needs to be less than the pivot and greater than the pivot on the right side. But inside the left and right it doesn't matter what order that are in.
+ * So the other really important thing is that this helper should do it in place. So it should not make a new array and it should return the index of the pivot so it returns the index. It does everything in place so don't make a new array to do it.
+ */
+function pivot(arr, start = 0, end = arr.length + 1) {
+  function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  var pivot = arr[start];
+  var swapIdx = start;
+
+  for (var i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+// Version with ES2015 Syntax
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+pivot([4, 8, 2, 1, 5, 7, 6, 3]); // return 3
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23]);
+
+// [4,6,9,1,2,5,3]
+// [3,2,1,4,6,9,5]
+//        4
+//  3,2,1    6,9,5
+//      3      6
+//  2,1      5  9
+//    2
+//  1
